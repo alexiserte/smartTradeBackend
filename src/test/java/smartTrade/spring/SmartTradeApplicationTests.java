79e742efa709f75420e92337d702aee72f1ba48f
alex1serte <alexiserte@gmail.com>
2024-03-15 17:49:15 +0100

    Añadidas dependencias de Spring

---
+++ src/test/java/smartTrade/spring/SmartTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package smartTrade.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
