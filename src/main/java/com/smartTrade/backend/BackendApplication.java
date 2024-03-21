a4a4583abe725e91f55d2805d2d7c4e29ee01e21
alex1serte <alexiserte@gmail.com>
2024-03-21 22:20:57 +0100

    Update pom.xml

--- src/main/java/com/smartTrade/backend/BackendApplication.java
+++ src/main/java/com/smartTrade/backend/BackendApplication.java
@@ -2,10 +2,19 @@ package com.smartTrade.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
+        @GetMapping("/mensaje")
+        
+        public String mensaje(){
+            return "Hello World";
+        }
+        
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
