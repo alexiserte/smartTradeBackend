8b263accf2d7a9c668e26fc77081473240ea7065
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-03-22 13:24:20 +0100

    Clase Consejo

---
+++ src/main/java/com/smartTrade/backend/models/Consejo.java
@@ -0,0 +1,27 @@
+package com.smartTrade.backend.models;
+
+public class Consejo {
+  private int consejo_id;
+  private int id_producto;
+  
+    public Consejo(int consejo_id, int id_producto) {
+        this.consejo_id = consejo_id;
+        this.id_producto = id_producto;
+    }
+
+    public int getConsejo_id() {
+        return consejo_id;
+    }
+
+    public void setConsejo_id(int consejo_id) {
+        this.consejo_id = consejo_id;
+    }
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+}
