a0c911189c72fb6a14399e8673a524ebd7059583
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-03-22 13:13:55 +0100

    Clases Carritos de Compra y LIsta de Deseos

---
+++ src/main/java/com/smartTrade/backend/models/CarritoCompra.java
@@ -0,0 +1,27 @@
+package com.smartTrade.backend.models;
+
+public class CarritoCompra {
+    private int id_carrito;
+    private int id_comprador;
+
+    public CarritoCompra(int id_carrito, int id_comprador) {
+        this.id_carrito = id_carrito;
+        this.id_comprador = id_comprador;
+    }
+
+    public int getId_carrito() {
+        return id_carrito;
+    }
+
+    public void setId_carrito(int id_carrito) {
+        this.id_carrito = id_carrito;
+    }
+
+    public int getId_comprador() {
+        return id_comprador;
+    }
+
+    public void setId_comprador(int id_comprador) {
+        this.id_comprador = id_comprador;
+    }
+}
---
+++ src/main/java/com/smartTrade/backend/models/ListaDeDeseos.java
@@ -0,0 +1,27 @@
+package com.smartTrade.backend.models;
+
+public class ListaDeDeseos {
+    private int id_lista_de_datos;
+    private int id_comprador;
+
+    public ListaDeDeseos(int id_lista_de_datos, int id_comprador) {
+        this.id_lista_de_datos = id_lista_de_datos;
+        this.id_comprador = id_comprador;
+    }
+
+    public int getId_lista_de_datos() {
+        return id_lista_de_datos;
+    }
+
+    public void setId_lista_de_datos(int id_lista_de_datos) {
+        this.id_lista_de_datos = id_lista_de_datos;
+    }
+
+    public int getId_comprador() {
+        return id_comprador;
+    }
+
+    public void setId_comprador(int id_comprador) {
+        this.id_comprador = id_comprador;
+    }
+}
