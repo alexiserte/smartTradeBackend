938ccfa6d88d829cb06a3602ec15bfefcd0fa06f
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-03-22 13:23:13 +0100

    Clase GuardarMasTarde

---
+++ src/main/java/com/smartTrade/backend/models/GuardarMasTarde.java
@@ -0,0 +1,27 @@
+package com.smartTrade.backend.models;
+
+public class GuardarMasTarde {
+    private int id_guardar_mas_tarde;
+    private int id_comprador;
+
+    public GuardarMasTarde(int id_guardar_mas_tarde, int id_comprador) {
+        this.id_guardar_mas_tarde = id_guardar_mas_tarde;
+        this.id_comprador = id_comprador;
+    }
+
+    public int getId_guardar_mas_tarde() {
+        return id_guardar_mas_tarde;
+    }
+
+    public void setId_guardar_mas_tarde(int id_guardar_mas_tarde) {
+        this.id_guardar_mas_tarde = id_guardar_mas_tarde;
+    }
+
+    public int getId_comprador() {
+        return id_comprador;
+    }
+
+    public void setId_comprador(int id_comprador) {
+        this.id_comprador = id_comprador;
+    }
+}
