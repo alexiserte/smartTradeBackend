fa1d006d096a903f1f33d5e0bc0fc77771abe54f
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-03-26 22:07:16 +0100

    Como funciona lo aplico a todos los casos

--- src/main/java/com/smartTrade/backend/BackendApplication.java
+++ src/main/java/com/smartTrade/backend/BackendApplication.java
@@ -3,6 +3,7 @@ package com.smartTrade.backend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,24 +46,26 @@ public class BackendApplication {
     @GetMapping("/comprador/")
     public ResponseEntity<?> comprador(@RequestParam(value = "id", required = false) Integer id_consumidor,
             @RequestParam(value = "nickname", required = false) String nickname) {
-        try{
-                if (id_consumidor != null && nickname != null) {
-            // Lógica para buscar por ID y nombre
-            return ResponseEntity.ok(compradorDAO.getCompradorByIDAndNombre(id_consumidor, nickname));
-        } else if (id_consumidor != null) {
-            // Lógica para buscar por ID
-            return ResponseEntity.ok(compradorDAO.getCompradorByID(id_consumidor));
-        } else if (nickname != null) {
-            // Lógica para buscar por nombre
-            return ResponseEntity.ok(compradorDAO.getCompradorByNombre(nickname));
-        } else {
-            // Manejar escenario sin parámetros
-            return ResponseEntity.ok(ResponseEntity.status(400).body("Número de parámetros incorrecto.")); // O lanzar una excepción, dependiendo de tu caso de uso
-        }
+        try {
+            if (id_consumidor != null && nickname != null) {
+                // Lógica para buscar por ID y nombre
+                return ResponseEntity.ok(compradorDAO.getCompradorByIDAndNombre(id_consumidor, nickname));
+            } else if (id_consumidor != null) {
+                // Lógica para buscar por ID
+                return ResponseEntity.ok(compradorDAO.getCompradorByID(id_consumidor));
+            } else if (nickname != null) {
+                // Lógica para buscar por nombre
+                return ResponseEntity.ok(compradorDAO.getCompradorByNombre(nickname));
+            } else {
+                return ResponseEntity.ok(ResponseEntity.status(400).body("Número de parámetros incorrecto.")); // O                                                                                           // de uso
+            }
+        } catch (EmptyResultDataAccessException e) {
+            return ResponseEntity.ok(ResponseEntity.status(404).body("Número de parámetros incorrecto."));
 
-    } catch (Exception e) {
-        return ResponseEntity.status(500).body("Error: " + e.getMessage());
-    }
+        } catch (Exception e) {
+
+            return ResponseEntity.ok(ResponseEntity.status(500).body("Error: " + e.getMessage()));
+        }
     }
 
     public static void main(String[] args) {
