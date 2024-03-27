663e1d1358ba2f663b03577a64e940c0301db78f
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-03-27 14:06:33 +0100

    Buenas prácticas API

--- src/main/java/com/smartTrade/backend/controlllers/CompradorController.java
+++ src/main/java/com/smartTrade/backend/controlllers/CompradorController.java
@@ -1,12 +1,19 @@
 package com.smartTrade.backend.controlllers;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.smartTrade.backend.daos.*;
+import com.smartTrade.backend.models.Comprador;
+
+import jakarta.annotation.PostConstruct;
 
 @RestController
 public class CompradorController {
@@ -15,30 +22,52 @@ public class CompradorController {
     CompradorDAO compradorDAO;
 
     @GetMapping("/comprador/")
-    public ResponseEntity<?> comprador(@RequestParam(value = "id", required = false) Integer id_consumidor,
-            @RequestParam(value = "nickname", required = false) String nickname) {
+    public ResponseEntity<?> login(@RequestParam(value = "nickname", required = true) String nickname,
+            @RequestParam(value = "password", required = true) String password) {
+
         try {
-            if (id_consumidor == null && nickname == null) {
-                return ResponseEntity.ok(compradorDAO.getAllCompradores());
-            } else if (id_consumidor != null && nickname != null) {
-                // Lógica para buscar por ID y nombre
-                return ResponseEntity.ok(compradorDAO.getCompradorByIDAndNombre(id_consumidor, nickname));
-            } else if (id_consumidor != null) {
-                // Lógica para buscar por ID
-                return ResponseEntity.ok(compradorDAO.getCompradorByID(id_consumidor));
-            } else if (nickname != null) {
-                // Lógica para buscar por nombre
-                return ResponseEntity.ok(compradorDAO.getCompradorByNombre(nickname));
-            } else {
-                return ResponseEntity.ok(ResponseEntity.status(400).body("Número de parámetros incorrecto.")); // O //
-                                                                                                               // de uso
-            }
+            Comprador comprador = compradorDAO.getCompradorByNicknameAndPassword(nickname, password);
+            return ResponseEntity.ok(comprador);
         } catch (EmptyResultDataAccessException e) {
-            return ResponseEntity.ok(ResponseEntity.status(404).body("Recurso no encontrado."));
-
-        } catch (Exception e) {
-
-            return ResponseEntity.ok(ResponseEntity.status(500).body("Error: " + e.getMessage()));
+            try {
+                compradorDAO.getCompradorByNombre(nickname); // Intenta obtener al comprador solo por nickname
+                return ResponseEntity.ok(ResponseEntity.status(400).body("Contraseña incorrecta."));
+            } catch (EmptyResultDataAccessException e2) {
+                return ResponseEntity.ok(ResponseEntity.status(404).body("Usuario no encontrado."));
+           }
         }
+    }
+
+    @PostMapping("/comprador/")
+    public ResponseEntity<?> register(@RequestParam(value = "nickname", required = true) String nickname,
+            @RequestParam(value = "password", required = true) String password) {
+
+        Random random = new Random();
+        int id = -1;
+        Comprador comprador;
+        boolean satisfied = false;
+
+        do{
+            try{
+                id = random.nextInt(1000);
+                comprador = compradorDAO.getCompradorByID(id);
+            }catch(EmptyResultDataAccessException e){
+                satisfied = true;
+            }
+        }while(satisfied == false);
+        try{
+            compradorDAO.insertCompradorOnlyNicknameAndPassword(id, nickname, password);
+            return ResponseEntity.ok(ResponseEntity.status(201).body("Usuario registrado correctamente."));
+        }catch(Exception e){
+            return ResponseEntity.ok(ResponseEntity.status(400).body("Error al registrar el usuario."));
+        }
+    }
+
+    @DeleteMapping("/comprador/")
+    public ResponseEntity<?> deleteComprador(@RequestParam(value = "id", required = true) int  id) {
+
+        //// EL METODO BORRAR COMPRADOR HAY QUE BORRAR TODO LO QUE REFERENCIA A ESA ID DE COMPRADOR
+            compradorDAO.deleteComprador(id);
+            return ResponseEntity.ok(ResponseEntity.status(200).body("Usuario eliminado correctamente."));
     }
 }
