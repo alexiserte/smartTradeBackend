65f64217067423312cb1a01ebb39272122862415
alex1serte <alexiserte@gmail.com>
2024-03-29 17:20:56 +0100

    Montando un buen readme.md

--- src/main/resources/ConsultasQueSePodrianUsar.sql
+++ src/main/resources/ConsultasQueSePodrianUsar.sql
@@ -3,3 +3,4 @@ SELECT p.nombre
 FROM producto p, detalle_pedido pe, comprador c, pedido ped
 WHERE c.nombre = ? AND ped.id_comprador = c.id_comprador AND pe.id_pedido = ped.id_pedido AND pe.id_articulo = p.id_articulo
 ORDER BY p.nombre
+
