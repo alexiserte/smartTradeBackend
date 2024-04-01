20a654d9c0a8823568688c522c307026b792dab2
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-01 14:40:11 +0200

    Corrección de errores

--- src/main/java/com/smartTrade/backend/mappers/DatabaseMapper.java
+++ src/main/java/com/smartTrade/backend/mappers/DatabaseMapper.java
@@ -1,18 +1,11 @@
 package com.smartTrade.backend.mappers;
-import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class DatabaseMapper implements ResultSetExtractor<List<String>> {
+public class DatabaseMapper implements RowMapper<String> {
     @Override
-    public List<String> extractData(ResultSet rs) throws SQLException {
-        List<String> databases = new ArrayList<>();
-        while (rs.next()) {
-            String databaseName = rs.getString(1); // Suponiendo que el nombre de la base de datos está en la primera columna
-            databases.add(databaseName);
-        }
-        return databases;
+    public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return rs.getString("name");
     }
 }
