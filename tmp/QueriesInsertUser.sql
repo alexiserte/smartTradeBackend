346b6ce4740dd9608ac55f86684a1677c23d42c3
alex1serte <alexiserte@gmail.com>
2024-04-05 17:37:14 +0200

    Arreglando el insert de usuario comprador

--- tmp/QueriesInsertUser.sql
+++ tmp/QueriesInsertUser.sql
@@ -5,6 +5,14 @@ VALUES(?,?,?,?,?);
 INSERT Comprador(id_usuario,puntos_responsabilidad)
 SELECT id,0 FROM Usuario WHERE nickname = ?;
 
+INSERT Carrito_Compra(id_comprador)
+SELECT id FROM Usuario WHERE nickname = ?;
+
+INSERT Guardar_Mas_Tarde(id_comprador)
+SELECT id FROM Usuario WHERE nickname = ?;
+
+INSERT Lista_De_Deseos(id_comprador)
+SELECT id FROM Usuario WHERE nickname = ?;
 -- INSERTAR NUEVO VENDEDOR
 INSERT Usuario(nickname,correo,user_password,direccion,fecha_registro)
 VALUES(?,?,?,?,?);
