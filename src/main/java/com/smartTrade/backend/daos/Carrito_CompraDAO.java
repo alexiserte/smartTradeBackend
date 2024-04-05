d76997a8fc62ebe056078711184a0cfc955315b3
alex1serte <alexiserte@gmail.com>
2024-04-05 19:55:06 +0200

    Prueba

---
+++ src/main/java/com/smartTrade/backend/daos/Carrito_CompraDAO.java
@@ -0,0 +1,26 @@
+package com.smartTrade.backend.daos;
+import com.smartTrade.backend.mappers.CompradorMapper;
+import com.smartTrade.backend.models.Comprador;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Date;
+
+@Repository
+public class Carrito_CompraDAO{
+    
+    private JdbcTemplate database;
+
+    public CompradorDAO(JdbcTemplate database) {
+        this.database = database;
+    }
+    
+
+    public void create(int id_comprador) {
+        database.update("INSERT INTO Carrito_Compra(id_comprador) VALUES (?)",id_comprador);
+    }
+    
+
+}
