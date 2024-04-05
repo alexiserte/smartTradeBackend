c3783ca29d3e8b3e96d1fbedb913f16c990ecf3d
alex1serte <alexiserte@gmail.com>
2024-04-05 19:57:28 +0200

    Prueba

--- tmp/QueriesInsertUser.sql
+++ tmp/QueriesInsertUser.sql
@@ -26,3 +26,23 @@ VALUES(?,?,?,?,?);
 
 INSERT Comprador(id_usuario,puntos_responsabilidad)
 SELECT id,0 FROM Usuario WHERE nickname = ?
+
+
+BEGIN TRANSACTION;
+
+INSERT INTO Usuario (nickname, correo, user_password, direccion, fecha_registro)
+VALUES (?, ?, ?, ?, ?);
+
+INSERT INTO Comprador (id_usuario, puntos_responsabilidad)
+SELECT id, 0 FROM Usuario WHERE nickname = ?;
+
+INSERT INTO Carrito_Compra (id_comprador)
+SELECT id FROM Usuario WHERE nickname = ?;
+
+INSERT INTO Guardar_Mas_Tarde (id_comprador)
+SELECT id FROM Usuario WHERE nickname = ?;
+
+INSERT INTO Lista_De_Deseos (id_comprador)
+SELECT id FROM Usuario WHERE nickname = ?;
+
+COMMIT;
