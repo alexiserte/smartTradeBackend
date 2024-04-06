9b9e2625186e343ed8e15571b065a1137f0ee311
alex1serte <alexiserte@gmail.com>
2024-04-06 03:51:33 +0200

    YA FUNCIONA CREAR USUARIOS

--- src/main/java/com/smartTrade/backend/daos/CompradorDAO.java
+++ src/main/java/com/smartTrade/backend/daos/CompradorDAO.java
@@ -55,7 +55,11 @@ public class CompradorDAO {
         Date fechaActual = new Date(System.currentTimeMillis());
         java.sql.Date fechaSQL = new java.sql.Date(fechaActual.getTime());
 
-        database.update("INSERT INTO Usuario(nickname, correo, user_password, direccion, fecha_registro) VALUES (?, ?, ?, ?, ?); INSERT INTO Comprador(id_usuario, puntos_responsabilidad) SELECT id, 0 FROM Usuario WHERE nickname = ?; INSERT INTO Carrito_Compra(id_comprador) SELECT id FROM Usuario WHERE nickname = ?; INSERT INTO Guardar_Mas_Tarde(id_comprador) SELECT id FROM Usuario WHERE nickname = ?; INSERT INTO Lista_De_Deseos(id_comprador) SELECT id FROM Usuario WHERE nickname = ?;", nickname, correo, password, direccion, fechaSQL, nickname, nickname, nickname, nickname);
+        database.update("INSERT INTO Usuario(nickname, correo, user_password, direccion, fecha_registro) VALUES (?, ?, ?, ?, ?);", nickname, correo, password, direccion, fechaSQL);
+        database.update("INSERT INTO Comprador(id_usuario, puntos_responsabilidad) SELECT id, 0 FROM Usuario WHERE nickname = ?;", nickname);
+        database.update("INSERT INTO Carrito_Compra(id_comprador) SELECT id FROM Usuario WHERE nickname = ?;", nickname);
+        database.update("INSERT INTO Guardar_Mas_Tarde(id_comprador) SELECT id FROM Usuario WHERE nickname = ?;", nickname);
+        database.update("INSERT INTO Lista_De_Deseos(id_comprador) SELECT id FROM Usuario WHERE nickname = ?;", nickname);
 
     }
 
