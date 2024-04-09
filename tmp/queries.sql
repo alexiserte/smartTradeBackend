1b580ad1962719a8666a2aaa6e3da0dde7bf65e2
alex1serte <alexiserte@gmail.com>
2024-04-09 21:45:02 +0200

    Trasvase de queries

--- tmp/queries.sql
+++ tmp/queries.sql
@@ -1,3 +1,5 @@
 SELECT nombre FROM Producto WHERE id_vendedor = ?
 
 SELECT nombre,id_vendedor,id_categoria,descripcion,precio FROM Producto WHERE id_categoria = ANY(SELECT id FROM Categoria WHERE id = ?)
+
+SELECT COUNT(*) FROM Producto WHERE nombre = ? AND id_vendedor = ANY(SELECT id FROM Vendedor WHERE nombre = ?)
