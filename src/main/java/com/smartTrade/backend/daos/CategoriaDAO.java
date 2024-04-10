5af33f74ee4dcf194537b3c6116a7073d83257f2
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-10 15:17:17 +0200

    Error solucionado

--- src/main/java/com/smartTrade/backend/daos/CategoriaDAO.java
+++ src/main/java/com/smartTrade/backend/daos/CategoriaDAO.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.smartTrade.backend.mappers.CategoriaMapper;
 import com.smartTrade.backend.models.Categoria;
 
+@Repository
 public class CategoriaDAO {
 
     private JdbcTemplate database;
