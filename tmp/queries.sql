2f0cee89740df37918e460206ffe99c205efd140
alex1serte <alexiserte@gmail.com>
2024-04-12 17:49:42 +0200

    Consulta para Jenn

--- tmp/queries.sql
+++ tmp/queries.sql
@@ -7,3 +7,5 @@ SELECT COUNT(*) FROM Producto WHERE nombre = ? AND id_vendedor = ANY(SELECT id F
 SELECT nombre, id_categoria, id_vendedor, precio, descripcion FROM Producto WHERE nombre = ? AND id_vendedor IN(SELECT id FROM Usuario WHERE nickname = ? AND id IN(SELECT id_usuario FROM Vendedor))
 
 SELECT SUM(cantidad) FROM Detalle_Pedido WHERE id_pedido = ANY(SELECT id FROM Pedido WHERE id_comprador = ANY(SELECT id_usuario FROM Comprador WHERE id_usuario = ANY(SELECT id FROM Usuario WHERE nickname = ?)))
+
+SELECT SUM(cantidad) FROM Detalle_Pedido WHERE id_producto = ANY(SELECT id FROM Producto WHERE id_vendedor = ANY(SELECT id_usuario FROM Vendedor WHERE id_usuario = ANY(SELECT id FROM Usuario WHERE nickname = ?)))
