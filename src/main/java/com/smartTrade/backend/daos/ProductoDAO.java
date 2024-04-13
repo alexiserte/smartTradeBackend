4070d82fa115c23accaa8d38cd1b8ddad8c9d53c
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-13 14:19:47 +0200

    Lógica Sistema de Precios

--- src/main/java/com/smartTrade/backend/daos/ProductoDAO.java
+++ src/main/java/com/smartTrade/backend/daos/ProductoDAO.java
@@ -84,7 +84,7 @@ public class ProductoDAO{
  * producto en la base de datos. Las claves en `HashMap` corresponden a los atributos del producto (por
  * ejemplo, "
  */
-    public void update(String nombre, String vendorName, HashMap<String, ?> atributos) {
+    public void update(String nombre, String vendorName, HashMap<String, ?> atributos) throws IllegalArgumentException {
         List<String> keys = new ArrayList<>(atributos.keySet());
         int id_vendedor = database.queryForObject(
                 "SELECT id_vendedor FROM Producto WHERE nombre = ? AND id_vendedor IN(SELECT id_usuario FROM Vendedor WHERE id_usuario IN(SELECT id FROM Usuario WHERE nickname = ?))",
