df7be29786a1b8a78a121c65b2b200cc26547fea
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-13 17:38:06 +0200

    Lógica Sistema de Precios

--- src/main/java/com/smartTrade/backend/daos/PrecioDAO.java
+++ src/main/java/com/smartTrade/backend/daos/PrecioDAO.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Repository;
 import java.util.HashMap;
+import java.util.List;
 
 @Repository
 public class PrecioDAO {
@@ -21,11 +22,18 @@ public class PrecioDAO {
         double preciominimo = database.queryForObject("SELECT MIN(precio) FROM Historico_Precios WHERE id_producto = ?",Double.class,id_product);
         double preciomaximo = database.queryForObject("SELECT MAX(precio) FROM Historico_Precios WHERE id_producto = ?",Double.class,id_product);
         double preciomedio = database.queryForObject("SELECT AVG(precio) FROM Historico_Precios WHERE id_producto = ?",Double.class,id_product);
+        List<Double> historicoPrecios = database.queryForList("SELECT precio FROM Historico_Precios WHERE id_producto = ?",Double.class,id_product);
+        HashMap<String,Double> precios = new HashMap<>();
+        for(int i = 0; i < historicoPrecios.size(); i++){
+            precios.put("Precio " + i, historicoPrecios.get(i));
+        }
         HashMap<String, Object> res = new HashMap<>();
+        res.put("Producto", productName);
         res.put("Número de cambios de precio", numerodecambios);
         res.put("Precio mínimo", preciominimo);
         res.put("Precio máximo", preciomaximo);
         res.put("Precio medio", preciomedio);
+        res.put("Histórico de precios", precios);
         return res;
     }
 }
