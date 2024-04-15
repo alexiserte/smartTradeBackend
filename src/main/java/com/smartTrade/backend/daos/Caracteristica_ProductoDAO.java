048c95247a2c3438bca10e74599d55ccaf6f112a
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-15 20:46:23 +0200

    Poniendo valores por defecto cuando no hay etiqueta inteligente

--- src/main/java/com/smartTrade/backend/daos/Caracteristica_ProductoDAO.java
+++ src/main/java/com/smartTrade/backend/daos/Caracteristica_ProductoDAO.java
@@ -1,5 +1,7 @@
 package com.smartTrade.backend.daos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -18,6 +20,9 @@ public class Caracteristica_ProductoDAO {
     public Caracteristica_ProductoDAO(JdbcTemplate database) {
         this.database = database;
     }
+
+    @Autowired
+    CaracteristicaDAO caracteristicaDAO;
 
     public void create(String nombre, String productName, String vendorName, String valor, String characteristicName) {
         int id_producto = database.queryForObject("SELECT id FROM Producto WHERE nombre = ? AND id_vendedor = (SELECT id_usuario FROM Vendedor WHERE id_usuario IN(SELECT id FROM Usuario WHERE nickname = ?));", Integer.class, productName, vendorName);
@@ -54,22 +59,44 @@ public class Caracteristica_ProductoDAO {
         database.update("DELETE FROM Caracteristica WHERE nombre = ? AND id_producto = ?;", nombre, id_producto);
     }
     
-    public HashMap<String,String> getSmartTag(String productName, String vendorName){
-        HashMap<String,String> res = new HashMap<>();
-        int id_producto = database.queryForObject("SELECT id FROM Producto WHERE nombre = ? AND id_vendedor = ANY(SELECT id_usuario FROM Vendedor WHERE id_usuario = ANY(SELECT id FROM Usuario WHERE nickname = ?))", Integer.class, productName, vendorName);
+    public HashMap<String, String> getSmartTag(String productName, String vendorName) {
+        List<String> listaDeCaracteristica = new ArrayList<>();
 
-        List<Caracteristica> queryResult = database.query("SELECT id_caracteristica, valor, id_categoria, id_producto FROM Caracteristica WHERE id_producto = ?", new CaracteristicaMapper(), id_producto);
-        
-        List<Object> valores = new ArrayList<>();
-        List<String> nombres = new ArrayList<>();
-        for(Caracteristica c : queryResult) {
-            valores.add(c.getValor());
-            String nombre = database.queryForObject("SELECT nombre FROM Caracteristicas_Categoria WHERE id = ?", String.class, c.getId_caracteristica());
-            nombres.add(nombre);
+        int id_categoria = database.queryForObject("SELECT id_categoria FROM Producto WHERE nombre = ? AND id_vendedor = ANY(SELECT id FROM Usuario WHERE  nickname = ?);", Integer.class, productName, vendorName);
+        String categoria = database.queryForObject("SELECT nombre FROM Categoria WHERE id = ?;", String.class, id_categoria);
+
+        listaDeCaracteristica = caracteristicaDAO.getCaracteristicasFromOneCategoria(categoria);
+
+        HashMap<String, String> default_map = new HashMap<>();
+
+        for (String s : listaDeCaracteristica) {
+            default_map.put(s, "Esta caracteristica no esta disponible para este producto.");
         }
 
-        for(int i = 0; i < nombres.size(); i++) {
-            res.put(nombres.get(i), valores.get(i).toString());
+        HashMap<String, String> res = new HashMap<>();
+        try {
+            int id_producto = database.queryForObject(
+                    "SELECT id FROM Producto WHERE nombre = ? AND id_vendedor = ANY(SELECT id_usuario FROM Vendedor WHERE id_usuario = ANY(SELECT id FROM Usuario WHERE nickname = ?))",
+                    Integer.class, productName, vendorName);
+
+            List<Caracteristica> queryResult = database.query(
+                    "SELECT id_caracteristica, valor, id_categoria, id_producto FROM Caracteristica WHERE id_producto = ?",
+                    new CaracteristicaMapper(), id_producto);
+
+            List<Object> valores = new ArrayList<>();
+            List<String> nombres = new ArrayList<>();
+            for (Caracteristica c : queryResult) {
+                valores.add(c.getValor());
+                String nombre = database.queryForObject("SELECT nombre FROM Caracteristicas_Categoria WHERE id = ?",
+                        String.class, c.getId_caracteristica());
+                nombres.add(nombre);
+            }
+
+            for (int i = 0; i < nombres.size(); i++) {
+                res.put(nombres.get(i), valores.get(i).toString());
+            }
+        } catch (EmptyResultDataAccessException e) {
+            return default_map;
         }
 
         res.remove("product");
