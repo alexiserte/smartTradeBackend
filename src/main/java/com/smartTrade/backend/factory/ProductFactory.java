b4d60eeaa4c44a1920c0f4f1e1decf288d307392
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-17 11:50:14 +0200

    Patrón fábrica

--- src/main/java/com/smartTrade/backend/factory/ProductFactory.java
+++ src/main/java/com/smartTrade/backend/factory/ProductFactory.java
@@ -2,11 +2,13 @@ package com.smartTrade.backend.factory;
 
 import com.smartTrade.backend.models.*;
 
+import java.util.List;
+
 public class ProductFactory{
 
     public static Producto getProduct(Product_Types productType, String nombre, int id_vendedor, double precio, String descripcion,
             int id_categoria, String imagen, java.sql.Date fecha_publicacion, boolean validado, int huella_ecologica,
-            Object... args) {
+            List<Object> args) {
         if (productType == null) {
             return null;
         }
@@ -25,33 +27,33 @@ public class ProductFactory{
                 return new Bebida(nombre, id_vendedor, precio, descripcion, id_categoria, imagen, fecha_publicacion,
                     validado, huella_ecologica);
             case DEPORTE:
-                if (args.length >= 4) {
+                if (args.size() >= 4) {
                     return new Deporte(nombre, id_vendedor, precio, descripcion, id_categoria, imagen, fecha_publicacion,
-                            validado, huella_ecologica, (String) args[0], (String) args[1], (String) args[2], (String) args[3]);
+                            validado, huella_ecologica, (String) args.get(0), (String) args.get(1), (String) args.get(2), (String) args.get(3));
                 }
                 break;
             case ELECTRONICA:
-                if (args.length >= 4) {
+                if (args.size() >= 4) {
                     return new Electronica(nombre, id_vendedor, precio, descripcion, id_categoria, imagen, fecha_publicacion,
-                            validado, huella_ecologica, (String) args[0], (String) args[1], (String) args[2], (String) args[3]);
+                            validado, huella_ecologica, (String) args.get(0), (String) args.get(1), (String) args.get(2), (String) args.get(3));
                 }
                 break;
             case FRESCOS:
-                if (args.length >= 2) {
+                if (args.size() >= 2) {
                     return new Frescos(nombre, id_vendedor, precio, descripcion, id_categoria, imagen, fecha_publicacion,
-                            validado, huella_ecologica, (String) args[1], (double) args[0]);
+                            validado, huella_ecologica, (String) args.get(0), (double) args.get(1));
                 }
                 break;
             case MODA:
-                if (args.length >= 5) {
+                if (args.size() >= 5) {
                     return new Moda(nombre, id_vendedor, precio, descripcion, id_categoria, imagen, fecha_publicacion, validado,
-                            huella_ecologica, (String) args[0], (String) args[1], (String) args[2], (String) args[3], (String) args[4]);
+                            huella_ecologica, (String) args.get(0), (String) args.get(1), (String) args.get(2), (String) args.get(3), (String) args.get(4));
                 }
                 break;
             case PROCESADOS:
-                if (args.length >= 2) {
+                if (args.size() >= 2) {
                     return new Procesados(nombre, id_vendedor, precio, descripcion, id_categoria, imagen, fecha_publicacion,
-                            validado, huella_ecologica, (String) args[0], (double) args[1]);
+                            validado, huella_ecologica, (String) args.get(0), (double) args.get(1));
                 }
                 break;
             default:
