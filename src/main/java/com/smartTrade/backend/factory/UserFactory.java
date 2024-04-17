6bc348aa25679134bd9a00a2666de58185490168
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-17 10:08:30 +0200

    Patrón fábrica

--- src/main/java/com/smartTrade/backend/factory/UserFactory.java
+++ src/main/java/com/smartTrade/backend/factory/UserFactory.java
@@ -4,29 +4,30 @@ import com.smartTrade.backend.models.Administrador;
 import com.smartTrade.backend.models.Usuario;
 import com.smartTrade.backend.models.Vendedor;
 import com.smartTrade.backend.models.Comprador;
+import com.smartTrade.backend.models.User_Types;
 
 public class UserFactory {
     
-    public Usuario createVoidUser(String type){
+    public Usuario createVoidUser(User_Types type){
         switch(type){
-            case "Administrador":
+            case ADMINISTRADOR:
                 return new Administrador();
-            case "Vendedor":
+            case VENDEDOR:
                 return new Vendedor();
-            case "Comprador":
+            case COMPRADOR:
                 return new Comprador();
             default:
                 return null;
         }
     }
 
-    public Usuario createUser(String type, String nickname, String password, String direccion, String correo){
+    public Usuario createUser(User_Types type, String nickname, String password, String direccion, String correo){
         switch(type){
-            case "Administrador":
+            case ADMINISTRADOR:
                 return new Administrador(nickname, password, direccion, correo);
-            case "Vendedor":
+            case VENDEDOR:
                 return new Vendedor(nickname, password, direccion, correo);
-            case "Comprador":
+            case COMPRADOR:
                 return new Comprador(nickname, password, direccion, correo, 0);
             default:
                 return null;
