f040cd45e4bd7e43ddcae96a0f19087c51a87639
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-17 11:44:44 +0200

    Patrón fábrica

--- src/main/java/com/smartTrade/backend/models/Product_Types.java
+++ src/main/java/com/smartTrade/backend/models/Product_Types.java
@@ -1,13 +1,23 @@
 package com.smartTrade.backend.models;
 
 public enum Product_Types {
-        ALIMENTACION,
-        BEBIDA,
-        COMIDA,
-        DEPORTE,
-        ELECTRONICA,
-        FRESCOS,
-        HIGIENE,
-        MODA,
-        PROCESADOS,
+        ALIMENTACION(1),
+        BEBIDA(9),
+        COMIDA(8),
+        DEPORTE(7),
+        ELECTRONICA(3),
+        FRESCOS(5),
+        HIGIENE(4),
+        MODA(2),
+        PROCESADOS(8);
+
+        private final int id;
+
+        Product_Types(int id) {
+          this.id = id;
+        }
+
+        public int getID() {
+            return id;
+        }
 }
