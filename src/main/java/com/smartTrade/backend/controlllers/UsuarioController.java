0b392aad385fd80b242632ed88c2fbbbb10c3410
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-19 13:40:19 +0200

    Primeros pasos para la lógica de login y register

--- src/main/java/com/smartTrade/backend/controlllers/UsuarioController.java
+++ src/main/java/com/smartTrade/backend/controlllers/UsuarioController.java
@@ -3,6 +3,7 @@ package com.smartTrade.backend.controlllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
@@ -24,7 +25,7 @@ import com.smartTrade.backend.models.Vendedor;
 
 @RestController
 public class UsuarioController {
-    
+
     @Autowired
     UsuarioDAO usuarioDAO;
 
@@ -39,57 +40,60 @@ public class UsuarioController {
 
     @GetMapping("/user/")
     private ResponseEntity<?> login(@RequestParam(name = "identifier", required = true) String identiFier,
-                                    @RequestParam(name = "password", required = true) String password) {
-       User_Types userType = usuarioDAO.whatTypeIs(identiFier);
+            @RequestParam(name = "password", required = true) String password) {
+        User_Types userType = usuarioDAO.whatTypeIs(identiFier);
 
-       class Response<T>{
+        class Response<T> {
             private T user;
             private User_Types userType;
 
-            public Response(T user, User_Types userType){
+            public Response(T user, User_Types userType) {
                 this.user = user;
                 this.userType = userType;
             }
 
-            public T getUser(){
+            public T getUser() {
                 return user;
             }
 
-            public User_Types getUserType(){
+            public User_Types getUserType() {
                 return userType;
             }
-       }
-       switch(userType){
-              case ADMINISTRADOR:
-                Administrador administrador = admin.readOne(identiFier);
-                if(administrador.getPassword().equals(password)){
-                    Response<Administrador> response = new Response<>(administrador, User_Types.ADMINISTRADOR);
-                    return ResponseEntity.ok(response);
-                }
-                else{
-                    return new ResponseEntity<>("Contraseña incorrecta",HttpStatus.UNAUTHORIZED);
-                }
-              case VENDEDOR:
-                Vendedor vendedor = vendedorDAO.readOne(identiFier);
-                if(vendedor.getPassword().equals(password)){
-                    Response<Vendedor> response = new Response<>(vendedor, User_Types.VENDEDOR);
-                    return ResponseEntity.ok(response);
-                }
-                else{
-                    return new ResponseEntity<>("Contraseña incorrecta",HttpStatus.UNAUTHORIZED);
-                }
-              case COMPRADOR:
-                Comprador comprador = compradorDAO.readOne(identiFier);
-                if(comprador.getPassword().equals(password)){
-                    Response<Comprador> response = new Response<>(comprador, User_Types.COMPRADOR);
-                    return ResponseEntity.ok(response);
-                }
-                else{
-                    return new ResponseEntity<>("Contraseña incorrecta",HttpStatus.UNAUTHORIZED);
-                }
-              default:
-                return new ResponseEntity<>("Nombre de usuario incorrecto",HttpStatus.NOT_FOUND);
-       }
+        }
+        try {
+            switch (userType) {
+                case ADMINISTRADOR:
+                    Administrador administrador = admin.readOne(identiFier);
+                    if (administrador.getPassword().equals(password)) {
+                        Response<Administrador> response = new Response<>(administrador, User_Types.ADMINISTRADOR);
+                        return ResponseEntity.ok(response);
+                    } else {
+                        return new ResponseEntity<>("Contraseña incorrecta", HttpStatus.UNAUTHORIZED);
+                    }
+                case VENDEDOR:
+                    Vendedor vendedor = vendedorDAO.readOne(identiFier);
+                    if (vendedor.getPassword().equals(password)) {
+                        Response<Vendedor> response = new Response<>(vendedor, User_Types.VENDEDOR);
+                        return ResponseEntity.ok(response);
+                    } else {
+                        return new ResponseEntity<>("Contraseña incorrecta", HttpStatus.UNAUTHORIZED);
+                    }
+                case COMPRADOR:
+                    Comprador comprador = compradorDAO.readOne(identiFier);
+                    if (comprador.getPassword().equals(password)) {
+                        Response<Comprador> response = new Response<>(comprador, User_Types.COMPRADOR);
+                        return ResponseEntity.ok(response);
+                    } else {
+                        return new ResponseEntity<>("Contraseña incorrecta", HttpStatus.UNAUTHORIZED);
+                    }
+                default:
+                    return new ResponseEntity<>("Usuario no encontrado", HttpStatus.NOT_FOUND);
+            }
+        } catch (EmptyResultDataAccessException e) {
+            return new ResponseEntity<>("Usuario no encontrado", HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error al obtener el usuario: " + e.getLocalizedMessage(),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
-
