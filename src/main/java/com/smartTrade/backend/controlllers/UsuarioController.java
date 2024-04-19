1673637c2da13bd33682e8a5d30f476af9625bc5
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-19 13:19:07 +0200

    Primeros pasos para la lógica de login y register

--- src/main/java/com/smartTrade/backend/controlllers/UsuarioController.java
+++ src/main/java/com/smartTrade/backend/controlllers/UsuarioController.java
@@ -4,14 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.smartTrade.backend.daos.AdministradorDAO;
+import com.smartTrade.backend.daos.CompradorDAO;
 import com.smartTrade.backend.daos.UsuarioDAO;
+import com.smartTrade.backend.daos.VendedorDAO;
+import com.smartTrade.backend.mappers.VendedorMapper;
 import com.smartTrade.backend.models.Administrador;
+import com.smartTrade.backend.models.Comprador;
 import com.smartTrade.backend.models.User_Types;
 import com.smartTrade.backend.models.Usuario;
+import com.smartTrade.backend.models.Vendedor;
 
 @RestController
 public class UsuarioController {
@@ -19,6 +27,14 @@ public class UsuarioController {
     @Autowired
     UsuarioDAO usuarioDAO;
 
+    @Autowired
+    CompradorDAO compradorDAO;
+
+    @Autowired
+    AdministradorDAO admin;
+
+    @Autowired
+    VendedorDAO vendedorDAO;
 
     @GetMapping("/user/")
     private ResponseEntity<?> login(@RequestParam(name = "identifier", required = true) String identiFier,
@@ -27,14 +43,26 @@ public class UsuarioController {
 
        switch(userType){
               case ADMINISTRADOR:
-                AdminController admin = new AdminController();
-                 return admin.loginAdministrador(identiFier, password);
+                Administrador administrador = admin.readOne(identiFier);
+                if(administrador.getPassword().equals(password)){
+                    MultiValueMap<String, String> headerHashMap = new LinkedMultiValueMap<>();
+                    headerHashMap.add("userType",User_Types.ADMINISTRADOR.toString());
+                    return ResponseEntity.ok(administrador);
+                }
               case VENDEDOR:
-              VendedorController vendor = new VendedorController();
-                return ResponseEntity.ok(vendor.getVendedor(identiFier, password));
+                Vendedor vendedor = vendedorDAO.readOne(identiFier);
+                if(vendedor.getPassword().equals(password)){
+                    MultiValueMap<String, String> headerHashMap = new LinkedMultiValueMap<>();
+                    headerHashMap.add("userType",User_Types.VENDEDOR.toString());
+                    return ResponseEntity.ok(vendedor);
+                }
               case COMPRADOR:
-              CompradorController comprador = new CompradorController();
-                return ResponseEntity.ok(comprador.login(identiFier, password));
+                Comprador comprador = compradorDAO.readOne(identiFier);
+                if(comprador.getPassword().equals(password)){
+                    MultiValueMap<String, String> headerHashMap = new LinkedMultiValueMap<>();
+                    headerHashMap.add("userType",User_Types.COMPRADOR.toString());
+                    return ResponseEntity.ok(comprador);
+                }
               default:
                 return ResponseEntity.badRequest().body("Los parámetros ingresados no corresponden a un usuario existente en la base de datos.");
        }
