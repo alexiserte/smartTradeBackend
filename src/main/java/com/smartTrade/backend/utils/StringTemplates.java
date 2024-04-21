1fcfe7a7857c7ff95e867944d89a312d8c996a5f
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-21 23:26:49 +0200

    Intentado poner las estadisticas de precio

--- src/main/java/com/smartTrade/backend/utils/StringTemplates.java
+++ src/main/java/com/smartTrade/backend/utils/StringTemplates.java
@@ -3,7 +3,7 @@ public class StringTemplates {
     public static final  String PRECIO_MINIMO = "¡Es el precio más bajo de %s!¡Aprovecha!";
     public static final  String PRECIO_MAXIMO = "¡Es el precio más alto de %s!¡Cuidado!";
     public static final String PRECIO_RECIENTE = "El precio ha disminuido";
-    public static final String PRECIO_DISMINUIDO = "El precio más bajo en %d días.";
+    public static final String PRECIO_DISMINUIDO = "El precio más bajo en %d día/s.";
     public static final String PRECIO_NORMAL = null;
     public static final String PRECIO_AUMENTADO = "El precio ha aumentado recientemente.";
     public static final String PRECIO_NOVEDAD = "¡Atención! ¡El precio acaba de bajar!";
