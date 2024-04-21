beb506d201ee423f68b369348e75ae9c6b23e380
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-21 19:00:17 +0200

    Intentado poner las estadisticas de precio

--- src/main/java/com/smartTrade/backend/daos/PrecioDAO.java
+++ src/main/java/com/smartTrade/backend/daos/PrecioDAO.java
@@ -28,7 +28,7 @@ public class PrecioDAO {
         double preciomedio = database.queryForObject("SELECT AVG(precio) FROM Historico_Precios WHERE id_producto = ?", Double.class, id_product);
     
         List<String> vendedores = database.queryForList("SELECT nickname FROM Usuario WHERE id IN (SELECT id_vendedor FROM Vendedores_Producto WHERE id_producto IN (SELECT id FROM Producto WHERE nombre = ?))", String.class, productName);
-        double precioActual = database.queryForObject("SELECT precio FROM Historico_Precios WHERE id_producto = ? ORDER BY fecha DESC LIMIT 1", Double.class, id_product);
+        double precioActual = database.queryForObject("SELECT precio FROM Historico_Precios WHERE id_producto = ? ORDER BY fecha_modificacion DESC LIMIT 1", Double.class, id_product);
         for (int j = 0; j < vendedores.size(); j++) {
             List<Double> preciosFromOneProduct = database.queryForList("SELECT precio FROM Historico_Precios WHERE id_producto = ? AND id_vendedor IN (SELECT id FROM Usuario WHERE nickname = ?)", Double.class, id_product, vendedores.get(j));
             TreeMap<String, Object> preciosVendedor = new TreeMap<>();
