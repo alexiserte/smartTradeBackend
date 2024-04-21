41e75622ad17bcd69b5fde02ee8d6239bbf93490
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-21 13:14:10 +0200

    Intentado poner las estadisticas de precio

--- src/main/java/com/smartTrade/backend/daos/PrecioDAO.java
+++ src/main/java/com/smartTrade/backend/daos/PrecioDAO.java
@@ -16,33 +16,35 @@ public class PrecioDAO {
 
     public HashMap<String, Object> getStats(String productName) {
         HashMap<String, Object> stats = new HashMap<>();
-        int id_product = database.queryForObject("SELECT id FROM Producto WHERE nombre = ?",Integer.class,productName);
-        double preciominimo = database.queryForObject("SELECT MIN(precio) FROM Historico_Precios WHERE id_producto = ?",Double.class,id_product);
-        double preciomaximo = database.queryForObject("SELECT MAX(precio) FROM Historico_Precios WHERE id_producto = ?",Double.class,id_product);
-        double preciomedio = database.queryForObject("SELECT AVG(precio) FROM Historico_Precios WHERE id_producto = ?",Double.class,id_product);
-        
+        int id_product = database.queryForObject("SELECT id FROM Producto WHERE nombre = ?", Integer.class, productName);
+        double preciominimo = database.queryForObject("SELECT MIN(precio) FROM Historico_Precios WHERE id_producto = ?", Double.class, id_product);
+        double preciomaximo = database.queryForObject("SELECT MAX(precio) FROM Historico_Precios WHERE id_producto = ?", Double.class, id_product);
+        double preciomedio = database.queryForObject("SELECT AVG(precio) FROM Historico_Precios WHERE id_producto = ?", Double.class, id_product);
+    
         List<String> vendedores = database.queryForList("SELECT nickname FROM Usuario WHERE id IN(SELECT id_vendedor FROM Vendedores_Producto WHERE id_producto IN(SELECT id FROM Producto WHERE nombre = ?))", String.class, productName);
-
-        
+    
         for (int j = 0; j < vendedores.size(); j++) {
-            List<Double> preciosFromOneProduct = database.queryForList("SELECT precio FROM Historico_Precios WHERE id_producto = ? AND id_vendedor IN(SELECT id FROM Usuario WHERE nickname = ?)",Double.class, id_product, vendedores.get(j));
+            List<Double> preciosFromOneProduct = database.queryForList("SELECT precio FROM Historico_Precios WHERE id_producto = ? AND id_vendedor IN(SELECT id FROM Usuario WHERE nickname = ?)", Double.class, id_product, vendedores.get(j));
             HashMap<String, Object> preciosVendedor = new HashMap<>();
             preciosVendedor.put("Vendedor", vendedores.get(j));
+            
             for (int i = 0; i < preciosFromOneProduct.size(); i++) {
                 preciosVendedor.put("Precio " + (i + 1), preciosFromOneProduct.get(i));
             }
-            preciosVendedor.put("Precio mínimo", database.queryForObject("SELECT MIN(precio) FROM Historico_Precios WHERE id_producto = ? AND id_vendedor IN(SELECT id FROM Usuario WHERE nickname = ?)",Double.class, id_product, vendedores.get(j)));
-            preciosVendedor.put("Precio máximo", database.queryForObject("SELECT MAX(precio) FROM Historico_Precios WHERE id_producto = ? AND id_vendedor IN(SELECT id FROM Usuario WHERE nickname = ?)",Double.class, id_product, vendedores.get(j)));
-            preciosVendedor.put("Precio promedio", database.queryForObject("SELECT AVG(precio) FROM Historico_Precios WHERE id_producto = ? AND id_vendedor IN(SELECT id FROM Usuario WHERE nickname = ?)",Double.class, id_product, vendedores.get(j)));
-            preciosVendedor.put("Número de cambios", database.queryForObject("SELECT COUNT(*) FROM Historico_Precios WHERE id_producto = ? AND id_vendedor IN(SELECT id FROM Usuario WHERE nickname = ?)",Integer.class, id_product, vendedores.get(j)));
+            
+            preciosVendedor.put("Precio mínimo", database.queryForObject("SELECT MIN(precio) FROM Historico_Precios WHERE id_producto = ? AND id_vendedor IN(SELECT id FROM Usuario WHERE nickname = ?)", Double.class, id_product, vendedores.get(j)));
+            preciosVendedor.put("Precio máximo", database.queryForObject("SELECT MAX(precio) FROM Historico_Precios WHERE id_producto = ? AND id_vendedor IN(SELECT id FROM Usuario WHERE nickname = ?)", Double.class, id_product, vendedores.get(j)));
+            preciosVendedor.put("Precio promedio", database.queryForObject("SELECT AVG(precio) FROM Historico_Precios WHERE id_producto = ? AND id_vendedor IN(SELECT id FROM Usuario WHERE nickname = ?)", Double.class, id_product, vendedores.get(j)));
+            preciosVendedor.put("Número de cambios", database.queryForObject("SELECT COUNT(*) FROM Historico_Precios WHERE id_producto = ? AND id_vendedor IN(SELECT id FROM Usuario WHERE nickname = ?)", Integer.class, id_product, vendedores.get(j)));
             
             stats.put("Vendedor " + (j + 1), preciosVendedor);
-            //AQUI DEBE IR EL MÉTODO QUE RETORNA DATOS CURIOSOS SOBRE EL PRECIO DE UN PRODUCTO
+            // AQUÍ DEBERÍA IR EL MÉTODO QUE RETORNA DATOS CURIOSOS SOBRE EL PRECIO DE UN PRODUCTO
         }
-
+    
         stats.put("Precio máximo general", preciomaximo);
         stats.put("Precio mínimo general", preciominimo);
         stats.put("Precio promedio general", preciomedio);
         return stats;
     }
+    
 }
