d9b07797389763af1d820b41327090dfad30794e
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-24 12:38:47 +0200

    Recuperar elementos de un carrito de la coimpra git push

--- src/main/java/com/smartTrade/backend/daos/Carrito_CompraDAO.java
+++ src/main/java/com/smartTrade/backend/daos/Carrito_CompraDAO.java
@@ -1,6 +1,12 @@
 package com.smartTrade.backend.daos;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.smartTrade.backend.mappers.ProductMapper;
+import com.smartTrade.backend.models.*;
 
 @Repository
 public class Carrito_CompraDAO{
@@ -15,6 +21,10 @@ public class Carrito_CompraDAO{
     public void create(String compradorName) {
         int id_comprador = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, compradorName);
         database.update("INSERT INTO Carrito_Compra(id_comprador) VALUES (?)",id_comprador);
+    }
+
+    public List<Producto> getCarritoFromUser(String nickname){
+        return database.query("SELECT nombre, id_categoria, descripcion, id_imagen,fecha_añadido,validado,huella_ecologica FROM Producto WHERE id IN(SELECT id_producto FROM Productos_Carrito WHERE id_carrito IN(SELECT id FROM Carrito_Compra WHERE id_comprador = ANY(SELECT id FROM Usuario WHERE nickname = ?)))",new ProductMapper(),nickname);
     }
     
 
