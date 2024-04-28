01c62cc7e5739a3c924cf097e46488615ead345a
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-28 22:40:19 +0200

    pom

--- src/main/java/com/smartTrade/backend/utils/QRGenerator.java
+++ src/main/java/com/smartTrade/backend/utils/QRGenerator.java
@@ -1,16 +1,28 @@
 package com.smartTrade.backend.Utils;
-import com.aspose.barcode.*;
-public class QRGenerator {
-    public void createQR() {
-        BarcodeGenerator gen = new BarcodeGenerator(EncodeTypes.QR, "Aspose");
-        gen.getParameters().getBarcode().getXDimension().setPixels(4);
-        generator.getParameters().setResolution(400);
-
-        // Establecer versión automática
-        gen.getParameters().getBarcode().getQR().setQrVersion(QRVersion.AUTO);
-
-        // Establecer el tipo de codificación QR de ForceMicroQR
-        gen.getParameters().getBarcode().getQR().setQrEncodeType(QREncodeType.FORCE_MICRO_QR);
-        gen.save(dataDir + "QREncodeTypeForceMicroQR.png", BarCodeImageFormat.PNG);
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+ 
+public class QRGenerator 
+{
+    public File generateQR(File file, String text, int h, int w) throws Exception
+    {
+       Charset charset = Charset.forName("ISO-8859-1");
+       CharsetEncoder encoder = charset.newEncoder();
+       byte[] b = null;
+       ByteBuffer bb = encoder.encode(CharBuffer.wrap(text));
+       b = bb.array();
+       String data = new String(b, "ISO-8859-1");
+       
+       BitMatrix matrix = null;
+       QRCodeWriter writer = new QRCodeWriter();
+       matrix = writer.encode(data, com.google.zxing.BarcodeFormat.QR_CODE, w, h);
+       MatrixToImageWriter.writeToFile(matrix, "PNG", file);
+       return file;
     }
 }
