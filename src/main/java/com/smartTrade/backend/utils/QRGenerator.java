4c5445db2bd4f8a6043d62e5e8e2e8ae296885a8
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-04-28 22:48:56 +0200

    pom

--- src/main/java/com/smartTrade/backend/utils/QRGenerator.java
+++ src/main/java/com/smartTrade/backend/utils/QRGenerator.java
@@ -7,6 +7,7 @@ import java.nio.charset.CharsetEncoder;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+
  
 public class QRGenerator 
 {
