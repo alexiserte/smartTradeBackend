b94c236c731c62e992b56bc4158cfa008264530e
alex1serte <alexiserte@gmail.com>
2024-05-01 01:41:36 +0200

    Borrar producto Guardar Más Tarde

--- src/main/java/com/smartTrade/backend/DAO/GuardarMasTardeDAO.java
+++ src/main/java/com/smartTrade/backend/DAO/GuardarMasTardeDAO.java
@@ -28,6 +28,14 @@ public class GuardarMasTardeDAO implements DAOInterface<Object>{
         database.update("INSERT INTO Productos_Guardar_Mas_Tarde(id_producto,id_guardar_mas_tarde,id_vendedor) VALUES (?,?,?)",id_producto,id_guardar_mas_tarde,id_vendedor);
     }
 
+    public void deleteProducto(String userNickname,String productName,String vendorName){
+        int id_comprador = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, userNickname);
+        int id_producto = database.queryForObject("SELECT id_producto FROM Producto WHERE nombre = ?", Integer.class, productName);
+        int id_vendedor = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, vendorName);
+        int id_guardar_mas_tarde = database.queryForObject("SELECT id FROM Guardar_Mas_Tarde WHERE id_comprador = ?", Integer.class, id_comprador);
+        database.update("DELETE FROM Productos_Guardar_Mas_Tarde WHERE id_producto = ? AND id_guardar_mas_tarde = ? AND id_vendedor = ?",id_producto,id_guardar_mas_tarde,id_vendedor);
+    }
+
     public void delete(Object ...args) {;}
     public void update(Object ...args) {;}
     public Object readOne(Object ...args) {return null;}
