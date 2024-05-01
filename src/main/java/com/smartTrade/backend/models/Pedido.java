fb6f4debc7e7ecac4ef6f1c393f7b140fd130848
alex1serte <alexiserte@gmail.com>
2024-05-01 19:17:07 +0200

    Rediseñando pedido

--- src/main/java/com/smartTrade/backend/models/Pedido.java
+++ src/main/java/com/smartTrade/backend/models/Pedido.java
@@ -1,48 +1,62 @@
 package com.smartTrade.backend.Models;
 
+import java.util.List;
+import java.sql.Date;
+
 public class Pedido{
+    private int id_comprador;
+    private List<Producto> productos;
+    private String estado;
+    private Date fecha_realizacion;
 
-    private int id_pedido;
-    private int id_consumidor;
-    private String articulo;
-    private int cantidad_de_producto;
-
-    public Pedido(int id_pedido, int id_consumidor, String articulo, int cantidad_de_producto) {
-        this.id_pedido = id_pedido;
-        this.id_consumidor = id_consumidor;
-        this.articulo = articulo;
-        this.cantidad_de_producto = cantidad_de_producto;
+    public Pedido(int id_comprador, List<Producto> productos, String estado, Date fecha_realizacion){
+        this.id_comprador = id_comprador;
+        this.productos = productos;
+        this.estado = estado;
+        this.fecha_realizacion = fecha_realizacion;
     }
 
-    public int getId_pedido() {
-        return id_pedido;
+    public Pedido(){}
+
+    public int getId_comprador() {
+        return id_comprador;
     }
 
-    public void setId_pedido(int id_pedido) {
-        this.id_pedido = id_pedido;
+    public void setId_comprador(int id_comprador) {
+        this.id_comprador = id_comprador;
     }
 
-    public int getId_consumidor() {
-        return id_consumidor;
+    public List<Producto> getProductos() {
+        return productos;
     }
 
-    public void setId_consumidor(int id_consumidor) {
-        this.id_consumidor = id_consumidor;
+    public String getProductosString(){
+        String res = "";
+        for(Producto p : productos){
+            res += p.getNombre() + " ";
+        }
+
+        return res;
     }
 
-    public String getArticulo() {
-        return articulo;
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
     }
 
-    public void setArticulo(String articulo) {
-        this.articulo = articulo;
+    public String getEstado() {
+        return estado;
     }
 
-    public int getCantidad_de_producto() {
-        return cantidad_de_producto;
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 
-    public void setCantidad_de_producto(int cantidad_de_producto) {
-        this.cantidad_de_producto = cantidad_de_producto;
+    public Date getFecha_realizacion() {
+        return fecha_realizacion;
     }
+
+    public void setFecha_realizacion(Date fecha_realizacion) {
+        this.fecha_realizacion = fecha_realizacion;
+    }
+
 }
