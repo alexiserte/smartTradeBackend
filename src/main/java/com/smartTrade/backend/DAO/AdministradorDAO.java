af0c1ac99f98ed99b47b3dd8135adcb6070b4121
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-05-05 13:34:32 +0200

    Mejorando legibilidad de CaracteristicaDAO.

--- src/main/java/com/smartTrade/backend/DAO/AdministradorDAO.java
+++ src/main/java/com/smartTrade/backend/DAO/AdministradorDAO.java
@@ -21,31 +21,6 @@ public class AdministradorDAO implements DAOInterface<Administrador>{
         this.database = database;
     }
 
-    /**
-     * El método "crear" inserta un nuevo usuario en la base de datos con el apodo,
-     * contraseña, correo
-     * electrónico, dirección y fecha de registro proporcionados, y también asigna
-     * al usuario como
-     * administrador.
-     * 
-     * @param nickname  El parámetro "apodo" suele ser un identificador único o
-     *                  nombre de usuario elegido
-     *                  por el usuario para su cuenta. Se utiliza para distinguir un
-     *                  usuario de otro en el sistema.
-     * @param password  El parámetro "contraseña" en el método "crear" es una cadena
-     *                  que representa la
-     *                  contraseña del usuario que se está creando.
-     * @param correo    El parámetro "correo" en el método representa la dirección
-     *                  de correo electrónico del
-     *                  usuario que se está creando. Es una variable de tipo cadena
-     *                  que almacena la dirección de correo
-     *                  electrónico proporcionada durante el proceso de creación del
-     *                  usuario.
-     * @param direccion El parámetro "direccion" en el método representa la
-     *                  dirección del usuario. Es una
-     *                  cadena que normalmente incluye información como dirección,
-     *                  ciudad, estado y código postal.
-     */
     @Transactional
     public void create(Object... args) {
         String nickname = (String) args[0];
@@ -62,66 +37,16 @@ public class AdministradorDAO implements DAOInterface<Administrador>{
 
     }
 
-    /**
-     * La función `readOne` recupera un objeto `Administrador` de la base de datos
-     * en función de un
-     * identificador determinado (apodo o correo electrónico).
-     * 
-     * @param identifier El parámetro "identificador" se utiliza para buscar un
-     *                   objeto "Administrador"
-     *                   específico según el apodo o la dirección de correo
-     *                   electrónico proporcionada. La consulta buscará un
-     *                   registro coincidente en la base de datos donde el `apodo` o
-     *                   `correo` (correo electrónico) coincida
-     *                   con el valor del `identificador`.
-     * @return Se devuelve un objeto `Administrador` desde el método `readOne`. El
-     *         método consulta la base
-     *         de datos en busca de campos específicos relacionados con un
-     *         administrador según el identificador
-     *         proporcionado (apodo o correo electrónico) y luego asigna los
-     *         resultados a un objeto "Administrador"
-     *         utilizando "AdministradorMapper".
-     */
     public Administrador readOne(Object ...args) {
         String identifier = (String) args[0];
         return database.queryForObject("SELECT u.nickname, u.correo, u.user_password,u.direccion,u.fecha_registro FROM Usuario u, Administrador a WHERE a.id_usuario = u.id AND (u.nickname = ? OR u.correo = ?)", new AdministradorMapper(), identifier, identifier);
     }
 
-    /**
-     * La función lee todos los objetos Comprador de la base de datos ejecutando una
-     * consulta SQL
-     * específica.
-     * 
-     * @return Se devuelve una lista de objetos Comprador. Los datos se recuperan de
-     *         la base de datos
-     *         ejecutando la consulta SQL que selecciona columnas específicas de la
-     *         tabla Usuario donde la
-     *         identificación coincide con la identificación en la tabla
-     *         Administrador. Luego, los resultados se
-     *         asignan a objetos Comprador utilizando CompradorMapper.
-     */
     public List<Administrador> readAll() {
         return database.query("SELECT u.nickname, u.correo, u.user_password,u.direccion,u.fecha_registro FROM Usuario u, Administrador a WHERE a.id_usuario = u.id", new AdministradorMapper());
     }
 
-    /**
-     * El método "actualizar" toma un apodo y un mapa de atributos, luego actualiza
-     * los valores
-     * correspondientes en la base de datos según el tipo de atributo.
-     * 
-     * @param nickname  El método "actualizar" que proporcionó toma un apodo y un
-     *                  mapa de atributos como
-     *                  parámetros. Itera sobre las claves en el mapa y actualiza el
-     *                  atributo correspondiente en la base de
-     *                  datos para el apodo dado.
-     * @param atributos El parámetro `atributos` es un mapa que contiene pares
-     *                  clave-valor donde la clave
-     *                  es un String que representa el atributo a actualizar en la
-     *                  base de datos y el valor es de tipo
-     *                  Objeto. El método itera sobre las claves en el mapa,
-     *                  recupera el valor correspondiente y, según el
-     *                  tipo de
-     */
+
     @SuppressWarnings("unchecked")
     public void update(Object... args) {
         String nickname = (String) args[0];
@@ -167,19 +92,6 @@ public class AdministradorDAO implements DAOInterface<Administrador>{
         }
     }
 
-    /**
-     * La función "eliminar" elimina un administrador y un usuario de la base de
-     * datos según el apodo
-     * proporcionado.
-     * 
-     * @param nickname El parámetro `nickname` en el método `delete` se utiliza para
-     *                 especificar el apodo
-     *                 del usuario cuyos registros deben eliminarse de la base de
-     *                 datos. El método primero borra el
-     *                 registro de la tabla `Administrador` donde el `id_usuario`
-     *                 coincide con el `id` del usuario con el
-     *                 especificado
-     */
     public void delete(Object... args) {
         String nickname = (String) args[0];
         database.update("DELETE FROM Administrador WHERE id_usuario = ANY(SELECT id FROM Usuario WHERE nickname = ?);",
--- src/main/java/com/smartTrade/backend/DAO/CaracteristicaDAO.java
+++ src/main/java/com/smartTrade/backend/DAO/CaracteristicaDAO.java
@@ -13,6 +13,7 @@ import com.smartTrade.backend.Models.Categoria_Caracteristica;
 
 @Repository
 public class CaracteristicaDAO implements DAOInterface<Categoria_Caracteristica>{
+    private static final String GET_ID_CATEGORIA = "SELECT id FROM Categoria WHERE nombre = ?;";
     private JdbcTemplate database;
 
     public CaracteristicaDAO (JdbcTemplate database) {
@@ -22,14 +23,14 @@ public class CaracteristicaDAO implements DAOInterface<Categoria_Caracteristica>
     public void create(Object ...args) {
         String nombre = (String) args[0];
         String characteristicName = (String) args[1];
-        int id_categoria = database.queryForObject("SELECT id FROM Categoria WHERE nombre = ?;", Integer.class, characteristicName);
+        int id_categoria = database.queryForObject(GET_ID_CATEGORIA, Integer.class, characteristicName);
         database.update("INSERT INTO Caracteristicas_Categoria(nombre, id_categoria) VALUES (?, ?);", nombre, id_categoria);
     }
 
     public Categoria_Caracteristica readOne(Object ...args) {
         String nombre = (String) args[0];
         String characteristicName = (String) args[1];
-        int id_categoria = database.queryForObject("SELECT id FROM Categoria WHERE nombre = ?;", Integer.class, characteristicName);
+        int id_categoria = database.queryForObject(GET_ID_CATEGORIA, Integer.class, characteristicName);
         return database.queryForObject("SELECT nombre, id_categoria FROM Caracteristicas_Categoria WHERE nombre = ? AND id_categoria = ?", new Categoria_CaracteristicaMapper(), nombre, id_categoria);
     }
 
@@ -44,7 +45,7 @@ public class CaracteristicaDAO implements DAOInterface<Categoria_Caracteristica>
         Map<String, Object> atributos = (HashMap<String, Object>) args[2];
         List<String> keys = new ArrayList<>(atributos.keySet());
         for (String key : keys) {
-            int id_categoria = database.queryForObject("SELECT id FROM Categoria WHERE nombre = ?;", Integer.class, characteristicName);
+            int id_categoria = database.queryForObject(GET_ID_CATEGORIA, Integer.class, characteristicName);
             Object valor = atributos.get(key);
             if (valor instanceof Integer) {
                 database.update("UPDATE Caracteristicas_Categoria SET " + key + " = ? WHERE nombre = ? AND id_categoria = ?;", (Integer) valor, nombre, id_categoria);
@@ -59,13 +60,13 @@ public class CaracteristicaDAO implements DAOInterface<Categoria_Caracteristica>
     public void delete(Object ...args) {
         String nombre = (String) args[0];
         String characteristicName = (String) args[1];
-        int id_categoria = database.queryForObject("SELECT id FROM Categoria WHERE nombre = ?;", Integer.class, characteristicName);
+        int id_categoria = database.queryForObject(GET_ID_CATEGORIA, Integer.class, characteristicName);
         database.update("DELETE FROM Caracteristicas_Categoria WHERE nombre = ? AND id_categoria = ?;", nombre, id_categoria);
     }
     
     public List<String> getCaracteristicasFromOneCategoria(String categoryName) {
         List<String> res  = new ArrayList<>();
-        int id_categoria = database.queryForObject("SELECT id FROM Categoria WHERE nombre = ?;", Integer.class, categoryName);
+        int id_categoria = database.queryForObject(GET_ID_CATEGORIA,Integer.class, categoryName);
         List<Categoria_Caracteristica> queryResult = database.query("SELECT nombre, id_categoria FROM Caracteristicas_Categoria WHERE id_categoria = ?", new Categoria_CaracteristicaMapper(), id_categoria);
         for(Categoria_Caracteristica c : queryResult) {
             res.add(c.getNombre());
