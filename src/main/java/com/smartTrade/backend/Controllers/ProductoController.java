a103841c8aa7dff4c8b0a7499ea4765ba44e6890
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-05-07 01:24:07 +0200

    Prueba

--- src/main/java/com/smartTrade/backend/Controllers/ProductoController.java
+++ src/main/java/com/smartTrade/backend/Controllers/ProductoController.java
@@ -5,8 +5,9 @@ import java.util.LinkedHashMap;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.sf.jsqlparser.expression.TryCastExpression;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.web.config.OffsetScrollPositionHandlerMethodArgumentResolverCustomizer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,37 +24,26 @@ import com.smartTrade.backend.Fachada.ProductoFachada;
 public class ProductoController {
 
     @Autowired
-    ProductoFachada fechada;
+    ProductoFachada fachada;
 
 
     @GetMapping("/productos/")
     public ResponseEntity<?> searchProductByName(@RequestParam(name = "name", required = true) String nombre,
             @RequestParam(name = "category", required = false) String category) {
-        return fechada.searchProductByName(nombre, category);
+        return fachada.searchProductByName(nombre, category);
     }
 
 
 
     @PostMapping("/producto/")
-    public ResponseEntity<?> insertarProducto(@RequestBody(required = true) Object body) {
-        if (body instanceof String) {
-            // If the body is a String, directly pass it to insertarProducto method
-            return fechada.insertarProducto((String) body);
-        } else if (body instanceof LinkedHashMap) {
-            // If the body is a LinkedHashMap, assume it's a JSON object
-            // Convert it to JSON string
-            try {
-                ObjectMapper objectMapper = new ObjectMapper();
-                String jsonString = objectMapper.writeValueAsString(body);
-                // Now jsonString contains the LinkedHashMap in JSON format
-                return fechada.insertarProducto(jsonString);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace(); // Handle the exception as needed
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing JSON");
-            }
-        } else {
-            // Handle unsupported body type here
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Unsupported body type");
+    public ResponseEntity<?> insertarProducto(@RequestBody(required = true) Object body) throws JsonProcessingException {
+        try {
+            String bodyString = (String) body;
+            return fachada.insertarProducto(bodyString);
+        } catch (Exception e) {
+            LinkedHashMap<String, String> response = new LinkedHashMap<>();
+            ObjectMapper objectMapper = new ObjectMapper();
+            return fachada.insertarProducto(objectMapper.writeValueAsString(response));
         }
     }
 
@@ -63,20 +53,20 @@ public class ProductoController {
     public ResponseEntity<?> deleteProductFromOneVendor(@PathVariable(name = "name", required = true) String productName,
             @PathVariable(name = "vendor", required = true) String vendorName) {
         
-        return fechada.deleteProductFromOneVendor(productName, vendorName);
+        return fachada.deleteProductFromOneVendor(productName, vendorName);
     }
     
 
     @DeleteMapping("/producto/")
     public ResponseEntity<?> deleteProduct(@RequestParam(name = "name", required = true) String productName) {
-        return fechada.deleteProduct(productName);
+        return fachada.deleteProduct(productName);
     }
     
 
     @PutMapping("/producto/")
     public ResponseEntity<?> updateProduct(@RequestParam(name = "name", required = true) String productName,
             @RequestBody(required = true) HashMap<String, ?> atributos) {
-        return fechada.updateProduct(productName, atributos);
+        return fachada.updateProduct(productName, atributos);
     }
     
 
@@ -84,7 +74,7 @@ public class ProductoController {
     public ResponseEntity<?> updateProductFromOneVendor(@PathVariable(name = "name", required = true) String productName,
             @PathVariable(name = "vendor", required = true) String vendorName,
             @RequestBody(required = true) HashMap<String, ?> atributos) {
-        return fechada.updateProductFromOneVendor(productName, vendorName, atributos);
+        return fachada.updateProductFromOneVendor(productName, vendorName, atributos);
     }
 
 
@@ -93,37 +83,37 @@ public class ProductoController {
             @RequestParam(name = "image", required = true) boolean image,
                                         @RequestParam(name = "oldMode", required = false) boolean oldMode) {
 
-        if(!oldMode)   return fechada.getProduct(productName,image);
-        else           return fechada.getOldProduct(productName);
+        if(!oldMode)   return fachada.getProduct(productName,image);
+        else           return fachada.getOldProduct(productName);
     }
 
     @GetMapping("/productos/vendedor/")
     public ResponseEntity<?> getProductsFromOneVendor(@RequestParam(name = "identifier", required = true) String vendorName){
-        return fechada.getProductsFromOneVendor(vendorName);
+        return fachada.getProductsFromOneVendor(vendorName);
     }
 
 
 
     @PutMapping("/producto/validar/")
     public ResponseEntity<?> validateProduct(@RequestParam(name = "name", required = true) String productName,@RequestParam(name = "vendor", required = true) String vendor){
-        return fechada.validarProducto(productName,vendor);
+        return fachada.validarProducto(productName,vendor);
     }
    
 
   
     @GetMapping("/producto/estadisticas/")
     public ResponseEntity<?> getStatistics(@RequestParam(name = "name", required = true) String productName){
-        return fechada.getEstadisticas(productName);
+        return fachada.getEstadisticas(productName);
     }
 
     @GetMapping("/producto/imagen/")
     public ResponseEntity<?> getImage(@RequestParam(name = "name", required = true) String productName){
-        return fechada.getImagen(productName);
+        return fachada.getImagen(productName);
     }
 
     @GetMapping("/productos/nombres")
     public ResponseEntity<?> getProductsNames(){
-        return fechada.productAllNames();
+        return fachada.productAllNames();
     }
 
     
