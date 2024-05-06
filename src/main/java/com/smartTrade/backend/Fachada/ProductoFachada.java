1a5d54f078c351d8830e498f323799060505f25c
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-05-07 00:28:33 +0200

    .

--- src/main/java/com/smartTrade/backend/Fachada/ProductoFachada.java
+++ src/main/java/com/smartTrade/backend/Fachada/ProductoFachada.java
@@ -90,7 +90,8 @@ public class ProductoFachada extends Fachada {
 
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
-            System.out.println("Error al insertar el producto: " + e.getLocalizedMessage());
+            System.out.println("Error al insertar el producto: " + e.getMessage());
+            System.out.println("Error al insertar el producto: " + e.getCause());
             return new ResponseEntity<>("Error al insertar el producto: " + e.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
