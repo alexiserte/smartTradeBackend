f130811339b3c5aa5696d25c80c28bbfbc8280e8
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-05-06 19:18:15 +0200

    hola git pus

--- src/main/java/com/smartTrade/backend/Fachada/ProductoFachada.java
+++ src/main/java/com/smartTrade/backend/Fachada/ProductoFachada.java
@@ -77,6 +77,7 @@ public class ProductoFachada extends Fachada {
 
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
+            System.out.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
