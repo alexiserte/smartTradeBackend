72f3d61e0a455d71635838d89a4cbf55890b93a7
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-05-07 12:41:05 +0200

    m

--- src/main/java/com/smartTrade/backend/DAO/ListaDeDeseosDAO.java
+++ src/main/java/com/smartTrade/backend/DAO/ListaDeDeseosDAO.java
@@ -20,7 +20,7 @@ public class ListaDeDeseosDAO implements DAOInterface<Object>{
 
     public void create(Object ...args) {
         String compradorName = (String) args[0];
-        int id_comprador = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, compradorName);
+        int id_comprador = database.queryForObject("SELECT id FROM Usuario WHERE nickname = ?", Integer.class, compradorName);
         database.update("INSERT INTO Lista_De_Deseos(id_comprador) VALUES (?)",id_comprador);
     }
 
