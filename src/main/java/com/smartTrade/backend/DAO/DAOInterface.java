91cd73a29fb37ac845d4c5f54963ef27ab0730e2
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-05-16 08:52:53 +0200

    Rediseñando la estructura de los DAO

--- src/main/java/com/smartTrade/backend/DAO/DAOInterface.java
+++ src/main/java/com/smartTrade/backend/DAO/DAOInterface.java
@@ -1,12 +1,13 @@
 package com.smartTrade.backend.DAO;
 
 import java.util.List;
+import java.util.Map;
 
 public interface DAOInterface<T>{
     // Operaciones CRUD a implementar por todos los DAO y cumplir con el patrón de Fábrica Abstracta
-    public void create(Object ...args);
-    public T readOne(Object ...args);
-    public List<? extends T> readAll();
-    public void update(Object ...args);
-    public void delete(Object ...args);
+    void create(Map<String, ?> args);
+    T readOne(Map<String, ?> args);
+    List<? extends T> readAll();
+    void update(Map<String, ?> args);
+    void delete(Map<String, ?> args);
 }
