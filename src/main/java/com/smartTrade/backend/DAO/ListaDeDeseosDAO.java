adf771565ba1fb3df4d76b036b5961a58b462a05
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-05-19 21:59:54 +0200

    Arreglado para Jenn

--- src/main/java/com/smartTrade/backend/DAO/ListaDeDeseosDAO.java
+++ src/main/java/com/smartTrade/backend/DAO/ListaDeDeseosDAO.java
@@ -38,15 +38,15 @@ public class ListaDeDeseosDAO implements DAOInterface<Object>{
     }
 
     public void deleteProducto(String userNickname,String productName,String vendorName){
-        int id_comprador = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, userNickname);
+        int id_comprador = database.queryForObject("SELECT id FROM Usuario WHERE nickname = ?", Integer.class, userNickname);
         int id_producto = database.queryForObject("SELECT id_producto FROM Producto WHERE nombre = ?", Integer.class, productName);
-        int id_vendedor = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, vendorName);
+        int id_vendedor = database.queryForObject("SELECT id FROM Usuario WHERE nickname = ?", Integer.class, vendorName);
         int id_lista_de_deseos = database.queryForObject("SELECT id FROM Lista_De_Deseos WHERE id_comprador = ?", Integer.class, id_comprador);
         database.update("DELETE FROM Productos_Lista_Deseos WHERE id_producto = ? AND id_lista_de_deseos = ? AND id_vendedor = ?",id_producto,id_lista_de_deseos,id_vendedor);
     }
 
     public void vaciarLista(String userNickname){
-        int id_comprador = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, userNickname);
+        int id_comprador = database.queryForObject("SELECT id  FROM Usuario WHERE nickname = ?", Integer.class, userNickname);
         int id_lista_de_deseos = database.queryForObject("SELECT id FROM Lista_De_Deseos WHERE id_comprador = ?", Integer.class, id_comprador);
         database.update("DELETE FROM Productos_Lista_Deseos WHERE id_id_lista_de_deseos = ?",id_lista_de_deseos);
 
