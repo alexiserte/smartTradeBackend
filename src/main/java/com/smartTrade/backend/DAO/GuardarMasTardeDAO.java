4a574cd83461297f6ddea315e5a4834e3b998b66
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2024-05-22 11:41:41 +0200

    Implementada logica de guardar mas tarde

--- src/main/java/com/smartTrade/backend/DAO/GuardarMasTardeDAO.java
+++ src/main/java/com/smartTrade/backend/DAO/GuardarMasTardeDAO.java
@@ -1,14 +1,30 @@
 package com.smartTrade.backend.DAO;
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
 
+import com.smartTrade.backend.Models.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class GuardarMasTardeDAO implements DAOInterface<Object>{
     
     private JdbcTemplate database;
+
+    @Autowired
+    CompradorDAO compradorDAO;
+
+    @Autowired
+    VendedorDAO vendedorDAO;
+
+    @Autowired
+    AdministradorDAO administradorDAO;
+
+    @Autowired
+    UsuarioDAO usuarioDAO;
 
     public GuardarMasTardeDAO(JdbcTemplate database) {
         this.database = database;
@@ -22,23 +38,23 @@ public class GuardarMasTardeDAO implements DAOInterface<Object>{
     }
 
     public void insertarProducto(String userNickname,String productName,String vendorName){
-        int id_comprador = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, userNickname);
+        int id_comprador = database.queryForObject("SELECT id FROM Usuario WHERE nickname = ?", Integer.class, userNickname);
         int id_producto = database.queryForObject("SELECT id_producto FROM Producto WHERE nombre = ?", Integer.class, productName);
-        int id_vendedor = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, vendorName);
+        int id_vendedor = database.queryForObject("SELECT id FROM Usuario WHERE nickname = ?", Integer.class, vendorName);
         int id_guardar_mas_tarde = database.queryForObject("SELECT id FROM Guardar_Mas_Tarde WHERE id_comprador = ?", Integer.class, id_comprador);
         database.update("INSERT INTO Productos_Guardar_Mas_Tarde(id_producto,id_guardar_mas_tarde,id_vendedor) VALUES (?,?,?)",id_producto,id_guardar_mas_tarde,id_vendedor);
     }
 
     public void deleteProducto(String userNickname,String productName,String vendorName){
-        int id_comprador = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, userNickname);
+        int id_comprador = database.queryForObject("SELECT id FROM Usuario WHERE nickname = ?", Integer.class, userNickname);
         int id_producto = database.queryForObject("SELECT id_producto FROM Producto WHERE nombre = ?", Integer.class, productName);
-        int id_vendedor = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, vendorName);
+        int id_vendedor = database.queryForObject("SELECT id FROM Usuario WHERE nickname = ?", Integer.class, vendorName);
         int id_guardar_mas_tarde = database.queryForObject("SELECT id FROM Guardar_Mas_Tarde WHERE id_comprador = ?", Integer.class, id_comprador);
         database.update("DELETE FROM Productos_Guardar_Mas_Tarde WHERE id_producto = ? AND id_guardar_mas_tarde = ? AND id_vendedor = ?",id_producto,id_guardar_mas_tarde,id_vendedor);
     }
 
     public void vaciarLista(String userNickname){
-        int id_comprador = database.queryForObject("SELECT id_usuario FROM Usuario WHERE nickname = ?", Integer.class, userNickname);
+        int id_comprador = database.queryForObject("SELECT id FROM Usuario WHERE nickname = ?", Integer.class, userNickname);
         int id_guardar_mas_tarde = database.queryForObject("SELECT id FROM Guardar_Mas_Tarde WHERE id_comprador = ?", Integer.class, id_comprador);
         database.update("DELETE FROM Productos_Guardar_Mas_Tarde WHERE id_guardar_mas_tarde = ?",id_guardar_mas_tarde);
 
@@ -46,7 +62,21 @@ public class GuardarMasTardeDAO implements DAOInterface<Object>{
 
     public void delete(Map<String,?> args) {;}
     public void update(Map<String,?> args) {;}
-    public Object readOne(Map<String,?> args) {return null;}
+    public Object readOne(Map<String,?> args) {
+        String compradorName = (String) args.get("compradorName");
+        int id_comprador = usuarioDAO.getID(compradorName);
+        int id_guardar_mas_tarde = database.queryForObject("SELECT id_guardar_mas_tarde FROM Guardar_Mas_Tarde WHERE id_comprador = ?", Integer.class, id_comprador);
+
+        List<Integer> id_productos = database.queryForList("SELECT id_producto FROM Productos_Guardar_Mas_Tarde WHERE id_guardar_mas_tarde = ?", Integer.class, id_guardar_mas_tarde);
+        List<Producto> productos = new ArrayList<>();
+        for(Integer id_product : id_productos){
+            Producto producto = database.queryForObject("SELECT nombre, id_categoria, descripcion, id_imagen, fecha_añadido, validado, huella_ecologica, stock, etiqueta_inteligente FROM Producto WHERE id = ?", Producto.class, id_product);
+            productos.add(producto);
+        }
+
+        return Map.of("compradorName",compradorName,"productos",productos);
+
+    }
     public List<Object> readAll() {return null;}
     
 
